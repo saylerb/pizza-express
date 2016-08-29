0b45f6ab4cb96b21a9ba6b735485aa9b2080f3a3
Brian Sayler <sayler.b@gmail.com>
2016-08-29 11:08:02 -0600

    server index.html in static assets folder

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
 const express = require('express')
 const app = express()
 
+const path = require('path')
+
+app.use(express.static('static'))
+
 app.set('port', process.env.PORT || 3000)
 
 app.locals.title = 'Pizza Express'
-
-app.get('/', (request, response) => {
-  response.send(app.locals.title)
-})
 
 if (!module.parent) {
   app.listen(app.get('port'), () => {
     console.log(`${app.locals.title} is running on ${app.get('port')}.`)
   })
 }
+
+app.get('/', (request, response) => {
+  response.sendFile(path.join(__dirname, '/static/index.html'))
+})
 
 module.exports = app
